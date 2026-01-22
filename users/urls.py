58b961a5a32220b96568b30b3e90261d7d386874
shubhamyadav-29 <shubhamarjun5@gmail.com>
2026-01-22 19:47:37 +0530

    Chapter 7: Generic View

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import  UserDetailAPIView
+from .views import  UserListAPIView
 
 urlpatterns = [
     # path('users/', UserListAPIView.as_view()),
-    path('users/<int:pk>/', UserDetailAPIView.as_view()),
+    path('users/<int:pk>/', UserListAPIView.as_view()),
 ]
--- users/views.py
+++ users/views.py
@@ -1,42 +1,50 @@
-from django.shortcuts import get_object_or_404
-from rest_framework.views import APIView 
-from rest_framework.response import Response
-from rest_framework import status
+# from django.shortcuts import get_object_or_404
+# from rest_framework.views import APIView 
+# from rest_framework.response import Response
+# from rest_framework import status
+# from .models import UserProfile
+# from .serializers import UserProfileSerializer
+
+# class UserDetailAPIView(APIView):
+#     def get(self , request,pk):
+#         user = get_object_or_404(UserProfile,pk=pk)
+#         serializer = UserProfileSerializer(user)
+#         return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    
+#     def put(self, request, pk):
+#         user = get_object_or_404(UserProfile,pk=pk)
+#         serializer = UserProfileSerializer(user,data=request.data)
+        
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(
+#                 serializer.data,
+#                 status=status.HTTP_200_OK
+#             )
+            
+            
+#         return Response(
+#             serializer.errors,
+#             status=status.HTTP_400_BAD_REQUEST
+#         )
+        
+#     def delete(self ,request, pk):
+#         user = get_object_or_404(UserProfile, pk=pk)
+#         user.delete()
+#         return Response(
+#             {"message": "User deleted successfully"},
+#             status=status.HTTP_204_NO_CONTENT
+#         )
+        
+
+from rest_framework.generics import ListCreateAPIView
 from .models import UserProfile
 from .serializers import UserProfileSerializer
 
-class UserDetailAPIView(APIView):
-    def get(self , request,pk):
-        user = get_object_or_404(UserProfile,pk=pk)
-        serializer = UserProfileSerializer(user)
-        return Response(serializer.data, status=status.HTTP_200_OK)
-    
-    
-    def put(self, request, pk):
-        user = get_object_or_404(UserProfile,pk=pk)
-        serializer = UserProfileSerializer(user,data=request.data)
+class UserListAPIView(ListCreateAPIView):
+    queryset= UserProfile.objects.all()
+    serializer_class =UserProfileSerializer
         
-        if serializer.is_valid():
-            serializer.save()
-            return Response(
-                serializer.data,
-                status=status.HTTP_200_OK
-            )
-            
-            
-        return Response(
-            serializer.errors,
-            status=status.HTTP_400_BAD_REQUEST
-        )
-        
-    def delete(self ,request, pk):
-        user = get_object_or_404(UserProfile, pk=pk)
-        user.delete()
-        return Response(
-            {"message": "User deleted successfully"},
-            status=status.HTTP_204_NO_CONTENT
-        )
-        
-            
         
  
