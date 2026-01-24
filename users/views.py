8e425952bd2a33f33e0cad95a0d5d66adcae59a7
shubhamyadav-29 <shubhamarjun5@gmail.com>
2026-01-24 14:04:33 +0530

    Chapter 8: creating a login api

--- users/views.py
+++ users/views.py
@@ -1,3 +1,52 @@
+from django.contrib.auth import authenticate
+from rest_framework.authtoken.models import Token
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+class LoginAPIView(APIView):
+    def post(self, request):
+        username = request.data.get("username")
+        password = request.data.get("password")
+
+        user = authenticate(username=username, password=password)
+
+        if not user:
+            return Response(
+                {"error": "Invalid credentials"},
+                status=status.HTTP_401_UNAUTHORIZED
+            )
+
+        token, _ = Token.objects.get_or_create(user=user)
+
+        return Response(
+            {"token": token.key},
+            status=status.HTTP_200_OK
+        )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # from django.shortcuts import get_object_or_404
 # from rest_framework.views import APIView 
 # from rest_framework.response import Response
@@ -38,16 +87,16 @@
 #         )
         
 
-from rest_framework import generics
-from .models import UserProfile
-from .serializers import UserProfileSerializer
+# from rest_framework import generics
+# from .models import UserProfile
+# from .serializers import UserProfileSerializer
 
-class UserListCreateAPIView(generics.ListCreateAPIView):
-    queryset = UserProfile.objects.all()
-    serializer_class = UserProfileSerializer
+# class UserListCreateAPIView(generics.ListCreateAPIView):
+#     queryset = UserProfile.objects.all()
+#     serializer_class = UserProfileSerializer
 
 
-class UserDetailAPIView(generics.RetrieveDestroyAPIView):
-    queryset = UserProfile.objects.all()
-    serializer_class = UserProfileSerializer
+# class UserDetailAPIView(generics.RetrieveDestroyAPIView):
+#     queryset = UserProfile.objects.all()
+#     serializer_class = UserProfileSerializer
  
