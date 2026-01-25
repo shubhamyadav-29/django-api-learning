7d48d6d65eedf5bfaf274bd67817ed801449f126
shubhamyadav-29 <shubhamarjun5@gmail.com>
2026-01-25 19:59:31 +0530

    .

--- users/urls.py
+++ users/urls.py
@@ -1,17 +1,7 @@
-# from django.urls import path
-# from .views import  UserListCreateAPIView , UserDetailAPIView
-
-# urlpatterns = [
-#     path('users/', UserListCreateAPIView.as_view()),
-#     path('users/<int:pk>/', UserDetailAPIView.as_view()),
-# ]
-
-
 from django.urls import path
-from .views import LoginAPIView
+from .views import UserListCreateAPIView, UserDetailAPIView
 
 urlpatterns = [
-    path('login/', LoginAPIView.as_view()),
     path('users/', UserListCreateAPIView.as_view()),
     path('users/<int:pk>/', UserDetailAPIView.as_view()),
 ]
