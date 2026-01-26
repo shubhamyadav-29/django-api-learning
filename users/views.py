8db6e4306a2bc62a1964ee42bb05b6aa67f9a91a
shubhamyadav-29 <shubhamarjun5@gmail.com>
2026-01-26 12:20:01 +0530

    debugging....

--- users/views.py
+++ users/views.py
@@ -2,9 +2,8 @@ from django.contrib.auth import authenticate
 from rest_framework.authtoken.models import Token
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import status,generics
 
-from rest_framework import generics
 from .models import UserProfile
 from .serializers import UserProfileSerializer
 
